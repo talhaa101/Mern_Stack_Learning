61670abbf5868ea926cde7c6390a6a2f87ae955e
Abu Talha <talha007abu@gmail.com>
2025-07-04 01:21:23 +0600

    first commit

---
+++ JS/naming.js
@@ -0,0 +1,14 @@
+let number = 10;
+
+// first_name 
+//firstName
+//FirstName
+//FIRSTNAME
+
+console.log (number);
+
+// a - z , 1-0 , _, $ 
+
+// naming concention 
+
+const TITLE = 'I love JS'
---
+++ JS/variable.js
@@ -0,0 +1,11 @@
+let firstName = 'Abu';
+var age = 25;
+let lastName = 'Talha';
+
+console.log(firstName);
+console.log(age);
+console.log(lastName);
+
+ const pi= 3.14159;
+
+ console.log(pi)
