2b52f899f30c481f4188008068e3549cc486e536
Abu Talha <talha007abu@gmail.com>
2025-07-04 13:22:47 +0600

    todays

---
+++ JS/data_types.js
@@ -0,0 +1,19 @@
+/**
+ * 1. Premitive value
+ *  a. String
+ *  b.Number
+ *  c. Boolean
+ *  d. undefined
+ *  e. null
+ *  f. BigInt
+ *  g.Symbol
+ * 2. Object
+ * 
+ * Check : typeof
+ * 
+ */
+
+
+let arr = ['one' , 'two' , 'three'];
+
+console.log(typeof arr);
